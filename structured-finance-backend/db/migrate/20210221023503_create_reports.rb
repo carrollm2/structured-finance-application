ee2c98846ededb6a20412cd940e9f27c3caddca3
carrollm2 <carrollm2@gmail.com>
2021-02-20 21:53:53 -0500

    create migration tables for transactions and reports

---
+++ structured-finance-backend/db/migrate/20210221023503_create_reports.rb
@@ -0,0 +1,13 @@
+class CreateReports < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reports do |t|
+      t.string :category
+      t.string :report_link
+      t.datetime :report_date
+      t.string :rating_agency
+      t.references :transaction, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
