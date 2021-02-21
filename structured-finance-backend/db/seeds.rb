f6e424f3481e0ad343ade638b4d8dfbcb455a17c
carrollm2 <carrollm2@gmail.com>
2021-02-20 22:48:24 -0500

    added seed data for testing purposes

--- structured-finance-backend/db/seeds.rb
+++ structured-finance-backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Deal.destroy_all
+Report.destroy_all
+
+
+deal_1 = Deal.create(ticker: 'JPPMT', series: '19-2', 'asset_class': 'rmbs')
+deal_1.reports.build(category: "Pre-Sale", report_link: "www.krba.com", rating_agency: "Kroll", report_date: DateTime.new(2021, 01, 15)).save
+deal_1.reports.build(category: "Downgrade", report_link: "wwww.moodys.com", rating_agency: "Moodys", report_date: DateTime.new(2020, 02, 15)).save
