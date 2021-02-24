b8f11d9eb1062c463602909702a1697d993fab41
carrollm2 <carrollm2@gmail.com>
2021-02-23 22:43:25 -0500

    fixed typo in inclusion validation for missing : before in

--- structured-finance-backend/app/models/deal.rb
+++ structured-finance-backend/app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < ApplicationRecord
     has_many :reports
 
-    validates_inclusion_of :asset_class, in => ['rmbs', 'cmbs', 'clo', 'auto', 'card']
+    validates_inclusion_of :asset_class, :in => ['rmbs', 'cmbs', 'clo', 'auto', 'card']
 end
