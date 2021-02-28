fda738a0b32d549e0b2f84670c57cee303dcd19c
carrollm2 <carrollm2@gmail.com>
2021-02-27 20:43:04 -0500

    testing actions with action object inside componentDidMount

--- structured-finance-frontend/src/App.js
+++ structured-finance-frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchDeals} from './actions/fetchDeals';
 
 class App extends React.Component{
 
   componentDidMount(){
-    fetch('http://127.0.0.1:3000/api/v1/deals/1/reports', {method: 'GET'})
-      .then(response => response.json())
-      .then(data => console.log(data))
+    // fetch('http://127.0.0.1:3000/api/v1/deals/1/reports', {method: 'GET'})
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    this.props.fetchDeals({type: 'FETCH_ACCOUNTS', payload: {name: 'Checking'}})
   }
 
   render(){
@@ -17,4 +20,4 @@ class App extends React.Component{
   }
 }
 
-export default App;
+export default connect(null, {fetchDeals})(App);
