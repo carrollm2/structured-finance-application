fcdd6d2ef860a22955ad5e3ff3300cd9ab2b178f
carrollm2 <carrollm2@gmail.com>
2021-02-27 20:39:49 -0500

    create actions folder

---
+++ structured-finance-frontend/src/actions/fetchDeals.js
@@ -0,0 +1,8 @@
+export function fetchDeals(action){
+
+    // fetch('http://127.0.0.1:3000/api/v1/deals/1/reports', {method: 'GET'})
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))    
+
+    return action
+}
