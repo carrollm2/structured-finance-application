00c2241a05709e415fa706b78805b05ffb721178
carrollm2 <carrollm2@gmail.com>
2021-02-27 20:42:01 -0500

    created store and redux devtools extension, nested App component inside Provider component

--- structured-finance-frontend/src/index.js
+++ structured-finance-frontend/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import {Provider} from 'react-redux'
 
+
+import {Provider} from 'react-redux';
 
 import App from './App';
+import dealReducer from './reducers/dealReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+let store = createStore(dealReducer, compose(applyMiddleware(thunk)));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
