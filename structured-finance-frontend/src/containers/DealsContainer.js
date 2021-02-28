d8dc2fb0e1c49125f69fefe1b2b95d97d0a11e7b
carrollm2 <carrollm2@gmail.com>
2021-02-28 13:57:43 -0500

    added DealsContainer for rendering DealInput and Deals components

---
+++ structured-finance-frontend/src/containers/DealsContainer.js
@@ -0,0 +1,26 @@
+import React from'react';
+import {connect} from 'react-redux';
+import {fetchDeals} from '../actions/fetchDeals';
+import Deals from '../components/Deals';
+import DealInput from '../components/DealInput';
+
+class DealsContainer extends React.Component{
+    componentDidMount(){
+        this.props.fetchDeals();
+    }
+
+    render (){
+        return (
+            <div>
+                <DealInput />
+                <Deals deals={this.props.deals}/>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    deals: state.deals
+})
+
+export default connect(mapStateToProps, {fetchDeals})(DealsContainer)
