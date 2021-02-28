40db600b8ad28987644faa07f683ae5ff2baacd2
carrollm2 <carrollm2@gmail.com>
2021-02-28 13:53:15 -0500

    after GET response and formatting reponse to json, fire action creator with dispatch for FETCH_DEALS

--- structured-finance-frontend/src/actions/fetchDeals.js
+++ structured-finance-frontend/src/actions/fetchDeals.js
@@ -1,8 +1,16 @@
+import { connect } from "react-redux"
+
 export function fetchDeals(action){
 
-    // fetch('http://127.0.0.1:3000/api/v1/deals/1/reports', {method: 'GET'})
-    //   .then(response => response.json())
-    //   .then(data => console.log(data))    
+      console.log('inside fetch deals');
+      return (dispatch) => {
+        fetch('http://127.0.0.1:3000/api/v1/deals', {method: 'GET'})
+        .then(response => response.json())  
+        .then(deals => dispatch({
+            type: "FETCH_DEALS",
+            payload: deals
+        }))
+      }  
 
-    return action
+
 }
