0c9fed056e85287ed6549d9e57d95503c96cef54
carrollm2 <carrollm2@gmail.com>
2021-03-03 00:23:24 -0500

    added new action, addDeal

---
+++ structured-finance-frontend/src/actions/addDeal.js
@@ -0,0 +1,15 @@
+export const addDeal = (data) => {
+    return (dispatch) => {
+        fetch('http://127.0.0.1:3000/api/v1/deals', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)       
+        })
+        .then(response => response.json())
+        .then(deal => dispatch({type: 'ADD_DEAL', payload: deal}))
+    }
+
+}
