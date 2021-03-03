cb8cc3915b564b663c0543447ed27f182a2a309d
carrollm2 <carrollm2@gmail.com>
2021-03-03 00:25:46 -0500

    added ADD_DEAL case to switch statement

--- structured-finance-frontend/src/reducers/dealReducer.js
+++ structured-finance-frontend/src/reducers/dealReducer.js
@@ -3,6 +3,8 @@ export default function dealReducer(state={deals: []}, action){
     switch (action.type){
         case 'FETCH_DEALS':
             return {deals: action.payload}
+        case 'ADD_DEAL':
+            return {...state, deals: [...state.deals, action.payload]}
         default:
             return state
     }
