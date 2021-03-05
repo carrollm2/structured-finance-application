7460ce3f92fb07f8bcd8ad0ad2b75157f6e04acd
carrollm2 <carrollm2@gmail.com>
2021-03-05 01:12:59 -0500

    nested App component inside Router component

--- structured-finance-frontend/src/index.js
+++ structured-finance-frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 
 import {Provider} from 'react-redux';
@@ -17,6 +18,8 @@ let store = createStore(dealReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>  
   </Provider>,
   document.getElementById('root'));
