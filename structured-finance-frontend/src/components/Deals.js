a477cca3f55342dfc3769684bb9b92497409a9eb
carrollm2 <carrollm2@gmail.com>
2021-03-05 01:16:28 -0500

    added Link component for each deal

--- structured-finance-frontend/src/components/Deals.js
+++ structured-finance-frontend/src/components/Deals.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
+import Deal from './Deal';
 
 const Deals = (props) => {
     return (
         <div>
-            {props.deals.map(deal => <li key={deal.id}> {deal.ticker} {deal.series} {deal.asset_class}</li>)}
+            {props.deals.map(deal => 
+                <div key={deal.id}>
+                    <Link to={`/deals/${deal.id}`}>{`${deal.ticker}`} {`${deal.series}`}</Link>
+                </div>)}
         </div>
     )
 }
