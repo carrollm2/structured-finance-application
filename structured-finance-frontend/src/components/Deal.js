415e6a1bb25cd410896f5b8ed02c922e0b135d10
carrollm2 <carrollm2@gmail.com>
2021-03-05 20:39:53 -0500

    return ReportsContainer and set ReportsContainer to have prop deal

--- structured-finance-frontend/src/components/Deal.js
+++ structured-finance-frontend/src/components/Deal.js
@@ -1,14 +1,18 @@
 import React from 'react'
 // import {Redirect} from 'react-router-dom';
+import ReportsContainer from '../containers/ReportsContainer'
 
 const Deal = (props) => {
 
     let deal = props.deals[props.match.params.id - 1]
 
     return (
-        <li>
-            {deal ? deal.ticker : null} - {deal ? deal.series : null} - {deal ? deal.asset_class : null}
-        </li>
+        <div>
+            <li>
+                {deal ? deal.ticker : null} - {deal ? deal.series : null} - {deal ? deal.asset_class : null}
+            </li>
+            <ReportsContainer deal={deal}/>
+        </div>
     )
 }
 
