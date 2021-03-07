dec88936c2e21e020007945ad42e44a589c2abc0
carrollm2 <carrollm2@gmail.com>
2021-03-07 13:51:52 -0500

    render reports for deal if there are reports to render

--- structured-finance-frontend/src/components/Reports.js
+++ structured-finance-frontend/src/components/Reports.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Reports = (props) => {
 
+    console.log(props)
     return (
         <div>
-            Reports
+            {props.reports && props.reports.map(report =>
+                <li key={report.id}>{report.catgeory} - {report.report_link} - {report.report_date} - {report.rating_agency}</li>)}
         </div>
     )
 
