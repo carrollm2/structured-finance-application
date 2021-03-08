112bffd7a021039c0601e859000c7f849ac35cf6
carrollm2 <carrollm2@gmail.com>
2021-03-07 22:51:14 -0500

    fixed fetch path for adding new report

--- structured-finance-frontend/src/actions/addReport.js
+++ structured-finance-frontend/src/actions/addReport.js
@@ -2,7 +2,7 @@ export const addReport = (report, dealId) => {
 
 
     return (dispatch) => {
-        fetch('http://127.0.0.1:3000/api/v1/deals/${dealId}/reports', {
+        fetch(`http://127.0.0.1:3000/api/v1/deals/${dealId}/reports`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application-json'
