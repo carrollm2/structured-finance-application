c7a08d1e51611161feed0511c8d88dfd73cb236c
carrollm2 <carrollm2@gmail.com>
2021-03-07 22:52:31 -0500

    added handleDelete for Delete button element

--- structured-finance-frontend/src/components/Reports.js
+++ structured-finance-frontend/src/components/Reports.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {deleteReport} from '../actions/deleteReport'
 
 const Reports = (props) => {
 
-    console.log(props)
+    // console.log(props)
+
+
+    const handleDelete = (report) => {
+        props.deleteReport(report.id, report.deal_id)
+    }    
+
+
     return (
         <div>
             {props.reports && props.reports.map(report =>
-                <li key={report.id}>{report.catgeory} - {report.report_link} - {report.report_date} - {report.rating_agency}</li>)}
+                <li key={report.id}>{report.catgeory} - {report.report_link} - {report.report_date} - {report.rating_agency}<button onClick={()=> handleDelete(report)}>Delete</button></li>)}
         </div>
     )
 
 }
 
-export default Reports
+export default connect(null, {deleteReport})(Reports)
