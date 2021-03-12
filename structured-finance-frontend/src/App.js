c6ad80dd1f8739c5d47efb9a1b5dcde2c2527db3
carrollm2 <carrollm2@gmail.com>
2021-03-12 08:51:29 -0500

    added Navbar component to App render

--- structured-finance-frontend/src/App.js
+++ structured-finance-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Navbar from './components/Navbar/Navbar';
 import DealsContainer from './containers/DealsContainer'
 
 class App extends React.Component{
@@ -7,6 +8,7 @@ class App extends React.Component{
   render(){
     return (
       <div className="App">
+        <Navbar />
         <DealsContainer />
       </div>
     );
