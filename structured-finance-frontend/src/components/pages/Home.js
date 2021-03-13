d6149f0f7b33eb1f2dea2fce3213a967a34f869b
carrollm2 <carrollm2@gmail.com>
2021-03-12 22:18:39 -0500

    add Footer Component to Home component

--- structured-finance-frontend/src/components/pages/Home.js
+++ structured-finance-frontend/src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../App.css'
 import HeroSection from '../Hero/HeroSection'
+import Footer from '../Footer/Footer'
 
 
 function Home(){
     return (
         <>
           <HeroSection />
+          <Footer />
         </>
     )   
 }
