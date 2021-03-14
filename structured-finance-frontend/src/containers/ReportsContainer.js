3d07a5b4eca07f832c2deceaf6746715677ee8b3
carrollm2 <carrollm2@gmail.com>
2021-03-14 12:09:09 -0400

    added spacing between element tags

--- structured-finance-frontend/src/containers/ReportsContainer.js
+++ structured-finance-frontend/src/containers/ReportsContainer.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReportInput from '../components/ReportInput'
 import Reports from '../components/Reports'
 
-
 class ReportsContainer extends React.Component{
 
     render(){
         return (
             <div>
                 <ReportInput deal={this.props.deal}/>
+                <br></br>
+                <h2> Latest Reports</h2>
+                <br></br>
                 <Reports reports={this.props.deal && this.props.deal.reports}/>
             </div>
         )
