9fb102b5be794e4a3b01b14c54c56aaf44a4677c
carrollm2 <carrollm2@gmail.com>
2021-03-14 11:55:00 -0400

    added catch error statement to fetch

--- structured-finance-frontend/src/actions/addReport.js
+++ structured-finance-frontend/src/actions/addReport.js
@@ -1,15 +1,15 @@
 export const addReport = (report, dealId) => {
-
-
     return (dispatch) => {
         fetch(`http://127.0.0.1:3000/api/v1/deals/${dealId}/reports`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application-json'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             },
             body: JSON.stringify(report)
         })
         .then(response => response.json())
         .then(report => dispatch({type: 'ADD_REPORT', payload: report}))
+        .catch(error => alert(error.message));
     }
 }
