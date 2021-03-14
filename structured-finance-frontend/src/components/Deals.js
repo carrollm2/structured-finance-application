515c7f42846120a22060fd251b55af8bd20651a2
carrollm2 <carrollm2@gmail.com>
2021-03-14 12:01:53 -0400

    used CardItem component to render each deal's data

--- structured-finance-frontend/src/components/Deals.js
+++ structured-finance-frontend/src/components/Deals.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import {Route, Link} from 'react-router-dom';
 import Deal from './Deal';
+import CardItem from './Card/CardItem'
+import './Card/Cards.css'
+import {Button} from './Button/Button.js'
+
 
 const Deals = (props) => {
     return (
-        <div>
-            {props.deals.map(deal => 
-                <div key={deal.id}>
-                    <Link to={`/deals/${deal.id}`}>{`${deal.ticker}`} {`${deal.series}`}</Link>
-                </div>)}
-        </div>
+        <>
+            <div className="cards__container">
+                {props.deals.map(deal => 
+                    <div className="cards_wrapper" key={deal.id}>
+                        <CardItem 
+                            icon={'fas fa-book'}
+                            text={`${deal.ticker} ${deal.series} Ratings Reports`}
+                            label={deal.asset_class}
+                            path={`/deals/${deal.id}`}
+                        />
+                    </div>)}
+                
+            </div>
+            <div className="create_new_deal">
+                <Link to="/deals/new">Add New Deal</Link>
+            </div>
+        </>
+
     )
 }
 export default Deals
