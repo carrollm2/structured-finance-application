7f05058aeee9b35be363f43e99cbd581d756db55
carrollm2 <carrollm2@gmail.com>
2021-03-14 11:51:31 -0400

    removed spacing between last import and imports above

--- structured-finance-frontend/src/App.js
+++ structured-finance-frontend/src/App.js
@@ -4,9 +4,6 @@ import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './components/App.css'
 import Home from './components/pages/Home'
-
-
-
 import DealsContainer from './containers/DealsContainer'
 
 class App extends React.Component{
