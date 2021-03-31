edc1e4698fdaece58760365fc6c3ff730600655c
carrollm2 <carrollm2@gmail.com>
2021-03-31 10:05:16 -0400

    converted function to ES6 syntax

--- structured-finance-frontend/src/components/Hero/HeroSection.js
+++ structured-finance-frontend/src/components/Hero/HeroSection.js
@@ -3,7 +3,8 @@ import '../App.css'
 import {Button} from '../Button/Button.js'
 import './HeroSection.css'
 
-function HeroSection(){
+
+const HeroSection = () => {    
     return (
         <div className='hero-container'>
             <video src='/videos/video-2.mp4' autoPlay loop muted />
